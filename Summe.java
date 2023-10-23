435bfe675b575228c13ed05d3c8a1e4bc5692d1a
omarchaouch17 <147279623+omarchaouch17@users.noreply.github.com>
2023-10-23 14:36:47 +0200

    Add files via upload

---
+++ Summe.java
@@ -0,0 +1,28 @@
+public class Summe{
+ public static void main(String[]args){
+
+  int n= Integer.parseInt(args[0]);
+  int ergebnis = 0;
+  for(int zahl=1;zahl!=n;zahl++){
+      ergebnis +=zahl;
+      }
+      System.out.println(ergebnis);
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+  }
